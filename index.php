49c1d4d92477bb0f9e26029a4b6b0eb39e8dcd17
Lee Ho Yean <comforest@naver.com>
2018-01-20 17:00:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
 	<link rel="stylesheet" href="/css/main.css">
-	<script type="text/javascript" src="rating.js"></script>
 </head>
 <body>
 	<?php
